07e4af2319ad5740544fff85a03df8edb443764b
Neksov <n2v@list.ru>
2020-08-11 10:59:44 +0300

    add new work

--- request.php
+++ request.php
@@ -21,7 +21,7 @@ try {
     $mail->Host       = 'smtp.yandex.ru';                    // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
     $mail->Username   = 'n2vvadim@yandex.ru';                     // SMTP username
-    $mail->Password   = '5104651046Vadim';                               // SMTP password
+    $mail->Password   = 'kiario1997';                               // SMTP password
     $mail->SMTPSecure = 'ssl';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
     $mail->Port       = 465;                                    // TCP port to connect to
     $mail->CharSet = "UTF-8";
